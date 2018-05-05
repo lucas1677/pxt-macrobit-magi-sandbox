6abb775df72cf280ce5e98dd7434a61f427e6c1d
lucas <lucas.shao@foxmail.com>
2018-05-05 11:03:37 +0800

    完成操作类方法的定义. 随后实现.

--- MagibitActuator.ts
+++ MagibitActuator.ts
@@ -13,14 +13,87 @@ namespace MagibitActuator {
         Forward,
         Backward
     }
+
+    export enum ServoPin {
+        P0,
+        P1,
+        P2,
+        P13,
+        P16
+    }
+
+    export enum LEDPin {
+        P0,
+        P1,
+        P2
+    }
+
+    export enum LEDState {
+        ON,
+        OFF
+    }
+
     /**
      * change motor's speed and direction
      */
     //% blockId=magibit_actuator_motor_set_speed
-    //% block="🍎 Motor |%motor| running with direction |%direction| and speed |%speed|"
+    //% block="Motor |%motor| running with direction |%direction| and speed |%speed|"
     //% weight=80
-    export function setMotorSpeed(motor: Motor, direction: MotorDirection, speed: number): void {
-
+    //% speed.min=0 speed.max=1023
+    export function motorSetSpeed(motor: Motor, direction: MotorDirection, speed: number): void {
+        return null;
     }
 
+
+    /**
+     * stop all motor
+     */
+    //% blockId=magibit_actuator_motor_stop_all
+    //% block="Motor stop all"
+    //% weight=80
+    export function motorStopAll(): void {
+        return null;
+    }
+
+    /**
+     * set servo angle
+     */
+    //% blockId=magibit_actuator_servo_set_angle
+    //% block="Servo |%pin| rotate to angle |%angle|°"
+    //% angle.min=0 angle.max=180
+    //% weight=80
+    export function servoSetAngle(pin: ServoPin, angle: number): void {
+        return null;
+    }
+
+    /**
+     * set LED'S brightness
+     */
+    //% blockId=magibit_actuator_led_set_brightness
+    //% block="LED |%pin| set brightness |%brightness|"
+    //% weight=80
+    //% brightness.min=0 brightness.max=255
+    export function ledSetBrightness(pin: LEDPin, brightness: number): void {
+        return null;
+    }
+
+    /**
+     * turn LED on or off
+     */
+    //% blockId=magibit_actuator_led_on_off
+    //% block="LED |%pin| |%state|"
+    //% weight=80
+    export function ledSetOnOff(pin: LEDPin, state: LEDState): void {
+        return null;
+    }
+
+    /**
+     * set LED'S brightness from A to B
+     */
+    //% blockId=magibit_actuator_led_set_brightness_range
+    //% block="Motor |%motor| running with direction |%direction| and speed |%speed|"
+    //% weight=80
+    export function ledSetBrightnessRange(motor: Motor, direction: MotorDirection, speed: number): void {
+        return null;
+    }
 }
