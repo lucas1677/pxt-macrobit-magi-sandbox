13fd9276746bcca462ec8b7db802951911ddd5c9
lucas <lucas.shao@foxmail.com>
2018-05-09 15:48:01 +0800

    simplify code structure.

--- MagibitActuator.ts
+++ MagibitActuator.ts
@@ -4,20 +4,49 @@
 //% weight=10 icon="\uf21c" color=#FF7210 block="Magibit Actuator"
 namespace actuator {
     /**
+     * 定义项目中使用的内部数据类型
+     */
+    export enum InnerNumberType {
+        ANALOG,
+        DIGITAL
+    }
+    export enum Motor {
+        M1,
+        M2
+    }
+
+    export enum MotorDirection {
+        Forward,
+        Backward
+    }
+
+    export enum LEDPin {
+        P0,
+        P1,
+        P2,
+        P13,
+        P16
+    }
+
+    export enum LEDState {
+        ON,
+        OFF
+    }
+    /**
      * change motor's speed and direction
      */
     //% blockId=magibit_actuator_motor_set_speed
     //% block="Motor |%motor| running with direction |%direction| and speed |%speed|"
     //% weight=80
     //% speed.min=0 speed.max=1023
-    export function motorSetSpeed(motor: magibit.Motor, direction: magibit.MotorDirection, speed: number): void {
+    export function motorSetSpeed(motor: Motor, direction: MotorDirection, speed: number): void {
 
-        let speedVal = magibit.filterInnerTypeNumber(magibit.InnerNumberType.ANALOG, speed);
+        let speedVal = filterInnerTypeNumber(InnerNumberType.ANALOG, speed);
 
-        if (motor == magibit.Motor.M1) {
+        if (motor == Motor.M1) {
             pins.digitalWritePin(DigitalPin.P8, direction);
             pins.analogWritePin(AnalogPin.P12, speedVal);
-        } else if (motor == magibit.Motor.M2) {
+        } else if (motor == Motor.M2) {
             pins.digitalWritePin(DigitalPin.P14, direction);
             pins.analogWritePin(AnalogPin.P15, speedVal)
         }
@@ -30,8 +59,8 @@ namespace actuator {
     //% block="Motor stop all"
     //% weight=80
     export function motorStopAll(): void {
-        motorSetSpeed(magibit.Motor.M1, magibit.MotorDirection.Forward, 0);
-        motorSetSpeed(magibit.Motor.M2, magibit.MotorDirection.Forward, 0);
+        motorSetSpeed(Motor.M1, MotorDirection.Forward, 0);
+        motorSetSpeed(Motor.M2, MotorDirection.Forward, 0);
     }
 
 
@@ -42,24 +71,24 @@ namespace actuator {
     //% block="LED |%pin| set brightness |%brightness|"
     //% weight=80
     //% brightness.min=0 brightness.max=1023
-    export function ledSetBrightness(pin: magibit.LEDPin, brightness: number): void {
+    export function ledSetBrightness(pin: LEDPin, brightness: number): void {
 
-        let brightnessVal = magibit.filterInnerTypeNumber(magibit.InnerNumberType.ANALOG, brightness);
+        let brightnessVal = filterInnerTypeNumber(InnerNumberType.ANALOG, brightness);
 
         switch (pin) {
-            case magibit.LEDPin.P0:
+            case LEDPin.P0:
                 pins.analogWritePin(AnalogPin.P0, brightnessVal);
                 break;
-            case magibit.LEDPin.P1:
+            case LEDPin.P1:
                 pins.analogWritePin(AnalogPin.P1, brightnessVal);
                 break;
-            case magibit.LEDPin.P2:
+            case LEDPin.P2:
                 pins.analogWritePin(AnalogPin.P2, brightnessVal);
                 break;
-            case magibit.LEDPin.P13:
+            case LEDPin.P13:
                 pins.analogWritePin(AnalogPin.P13, brightnessVal);
                 break;
-            case magibit.LEDPin.P16:
+            case LEDPin.P16:
                 pins.analogWritePin(AnalogPin.P16, brightnessVal);
                 break;
         }
@@ -71,14 +100,42 @@ namespace actuator {
     //% blockId=magibit_actuator_led_on_off
     //% block="LED |%pin| |%state|"
     //% weight=80
-    export function ledSetOnOff(pin: magibit.LEDPin, state: magibit.LEDState): void {
+    export function ledSetOnOff(pin: LEDPin, state: LEDState): void {
         switch (state) {
-            case magibit.LEDState.ON:
+            case LEDState.ON:
                 ledSetBrightness(pin, 1023);
                 break;
-            case magibit.LEDState.OFF:
+            case LEDState.OFF:
                 ledSetBrightness(pin, 0);
                 break;
         }
+    }
+
+    /**
+     * 项目内部的数据类型非法制过滤
+     * @param {InnerNumberType} innerType
+     * @param {number} analogNumber
+     * @returns {number}
+     */
+    export function filterInnerTypeNumber(innerType: InnerNumberType, analogNumber: number): number {
+        switch (innerType) {
+            case InnerNumberType.ANALOG: {
+                if (analogNumber < 0 || analogNumber == null) {
+                    return 0;
+                } else if (analogNumber > 1023) {
+                    return 1023;
+                } else {
+                    return analogNumber;
+                }
+            }
+            case InnerNumberType.DIGITAL: {
+                if (analogNumber < 0 || analogNumber == null) {
+                    return 0;
+                } else {
+                    return analogNumber;
+                }
+            }
+        }
+        return 0;
     }
 }
